2da74a90d8c52675c9ef2d1348cd07dc6109baee
Brian Han <bmhan319@hotmail.com>
2020-10-17 16:08:54 -0700

    added mobile header and collapsable menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Header from './components/Header'
+import MobileMenu from './components/MobileMenu'
+import './css/header.css'
+import './css/mobile-menu.css'
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    menuOpen: false
+  }
+
+  menu =() => {
+    document.getElementById("mobileMenu").style.display = (this.state.menuOpen === false) ? "block" : "none"
+    if (this.state.menuOpen === false) {
+      this.setState({menuOpen: true})
+    } else {
+      this.setState({menuOpen: false})
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header menu={this.menu} />
+        <MobileMenu />
+        <div>{this.state.menuOpen}</div>
+      </div>
+    );
+   }
 }
 
 export default App;
