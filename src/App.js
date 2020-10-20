b4f05438949e6e8aa3f30576a18ff01f4d13af62
Brian Han <bmhan319@hotmail.com>
2020-10-19 22:00:38 -0700

    adjusted js code some more, added comments to each function

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import './css/footer.css'
 
 const TOKEN = process.env.REACT_APP_BITLY_TOKEN
 
-
 class App extends Component {
   state = {
     menuOpen: false,
@@ -25,7 +24,7 @@ class App extends Component {
     bitly: []
   }
 
- 
+  //Open and close hamburger menu within the mobile site
   menu = () => {
     document.getElementById("mobileMenu").style.display = (this.state.menuOpen === false) ? "block" : "none"
     if (this.state.menuOpen === false) {
@@ -35,22 +34,24 @@ class App extends Component {
     }
   }
 
+  //Takes user input and error checks and correctly formats text then sends it out for an API call
   submitLink = (e) => {
     e.preventDefault()
     const link = document.getElementById("inputText").value
+    const prefix = /http:\/\//gi
+    const prefixSecure = /https:\/\//gi
+    const formattedLink = ( link.match(prefix) || link.match(prefixSecure) ) ? link : "https://" + link
+
     if (link.length === 0) {
       console.log('error')
+      //Need to write code to handle error input
     } else {
-      this.callAPI(link)
+      this.callAPI(formattedLink)
     }
   }
 
-
-  callAPI = async (text) => {
-    const prefix = /http:\/\//gi
-    const prefixSecure = /https:\/\//gi
-    let formattedLink = ( text.match(prefix) || text.match(prefixSecure) ) ? text : "https://" + text
-    
+  //Makes call to Bitly API
+  callAPI = async (formattedLink) => {
     const bitly = "https://api-ssl.bitly.com/v4/shorten"
     const setting = {
       method: 'POST',
@@ -60,42 +61,49 @@ class App extends Component {
       },
       body: JSON.stringify({ "long_url": formattedLink })
     }
+
+    //Request data from Bitly
     const call = await fetch(bitly, setting)
 
+    //Receive data and add data to State
     try {
       const data = await call.json()
       this.setState({
         longLink: data.long_url,
-        shortLink: data.link
+        shortLink: data.link,
+        count: this.state.count + 1
       })
     } catch(err) {
       console.error(err)
     }
 
+    //Adjusts the bitly array state by adding in newly aquired API data
     this.setState({
-      count: this.state.count + 1,
       bitly: [ ...this.state.bitly, [this.state.count, this.state.longLink, this.state.shortLink] ]
     })
   }
 
-
+  //Copies selected shortlink to clipboard
   copy = (num, shortLink) => {
     const button = document.getElementById(`copyButton${num}`)
     const buttons = document.querySelectorAll('.copyButton')
     const dummy = document.createElement("textarea");
     
+    //Create temp text input in order to select link and run 'copy' command
     document.body.appendChild(dummy);
     dummy.value = shortLink;
     dummy.select();
     document.execCommand("copy")
     document.body.removeChild(dummy);
     
+    //Reset all copy button elements ro default color and text
     buttons.forEach( item => {
       item.innerHTML = "Copy"
       item.classList.remove('colorActive')
       item.classList.add('colorDefault')
     })
     
+    //Change color and text of the selected copy button to reflect 'copy' action
     button.innerHTML = "Copied!"
     button.classList.remove('colorDefault')
     button.classList.add('colorActive')
