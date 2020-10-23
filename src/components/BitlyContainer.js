9e65acfe690031b1d03df6abc689c675e9c3c869
Brian Han <bmhan319@hotmail.com>
2020-10-22 19:38:22 -0700

    style form and active link container for desktop

--- src/components/BitlyContainer.js
+++ src/components/BitlyContainer.js
@@ -7,9 +7,11 @@ export default function BitlyContainer(props) {
         <li className="bitlyLinks" key={item[0]}>
           <p className="linkText linkLong">{item[1]}</p>
           <hr className="linkLine"/>
-          <p className="linkText linkShort">{item[2]}</p>
-          <div className="linkButtonContainer">
-            <button id={ `copyButton${item[0]}` } onClick={ ()=>{props.copy(item[0], item[2])} } className="copyButton colorDefault">Copy</button>
+          <div className="desktopContainer">
+            <p className="linkText linkShort">{item[2]}</p>
+            <div className="linkButtonContainer">
+              <button id={ `copyButton${item[0]}` } onClick={ ()=>{props.copy(item[0], item[2])} } className="copyButton colorDefault">Copy</button>
+            </div>
           </div>
         </li>
       )) }
